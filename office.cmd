a810ff4d9e1d9af77cf7a561d83829cc619fa61c
Amit Sharma <137687459+Amit123456777@users.noreply.github.com>
2024-05-26 00:32:35 +0530

    Add files via upload

---
+++ office.cmd
@@ -0,0 +1,85 @@
+C:\Windows\system32>cd /d %ProgramFiles%\Microsoft Office\Office16
+ C:\Program Files\Microsoft Office\Office16>cd /d %ProgramFiles(x86)%\Microsoft Office\Office16
+ The system cannot find the path specified.
+ C:\Program Files\Microsoft Office\Office16>for /f %x in ('dir /b ..\root\Licenses16\ProPlus2019VL*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_KMS_Client_AE-ppd.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_kms_client_ae-ppd.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_KMS_Client_AE-ul-oob.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_kms_client_ae-ul-oob.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_KMS_Client_AE-ul.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_kms_client_ae-ul.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_MAK_AE-pl.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_mak_ae-pl.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_MAK_AE-ppd.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_mak_ae-ppd.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_MAK_AE-ul-oob.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_mak_ae-ul-oob.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inslic:"..\root\Licenses16\ProPlus2019VL_MAK_AE-ul-phn.xrm-ms"
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installing Office license: ..\root\licenses16\proplus2019vl_mak_ae-ul-phn.xrm-ms
+ Office license installed successfully.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /setprt:1688
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Successfully applied setting.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /unpkey:6MWKP >nul
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /inpkey:NMMKJ-6RK4F-KMJVX-8D9MJ-6MWKP
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /sethst:107.175.77.7
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Successfully applied setting.
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>cscript ospp.vbs /act
+ Microsoft (R) Windows Script Host Version 5.812
+ Copyright (C) Microsoft Corporation. All rights reserved.
+ ---Processing--------------------------
+ Installed product key detected - attempting to activate the following product:
+ SKU ID: 85dd8b5f-eaa4-4af3-a628-cce9e77c9a03
+ LICENSE NAME: Office 19, Office19ProPlus2019VL_KMS_Client_AE edition
+ LICENSE DESCRIPTION: Office 19, VOLUME_KMSCLIENT channel
+ Last 5 characters of installed product key: 6MWKP
+
+
+ ---Exiting-----------------------------
+ C:\Program Files\Microsoft Office\Office16>
